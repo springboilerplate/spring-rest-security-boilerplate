b374f953adc9429c373c9c8b918efc9631ac4e2a
Volkan <volkandogan.d@gmail.com>
2017-11-03 10:52:35 +0300

    First commit of the project

---
+++ src/test/java/com/springrestsecurityboilerplate/SpringRestSecurityBoilerplateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springrestsecurityboilerplate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestSecurityBoilerplateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
