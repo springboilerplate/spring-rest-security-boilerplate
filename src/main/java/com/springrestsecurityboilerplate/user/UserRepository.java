86f77a5fc33e39ef143f39d12bf483cfb9da9fff
Volkan <volkandogan.d@gmail.com>
2017-11-04 11:54:54 +0300

    Entity, Database and Repo

---
+++ src/main/java/com/springrestsecurityboilerplate/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.springrestsecurityboilerplate.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Long> {
+	
+	
+
+}
